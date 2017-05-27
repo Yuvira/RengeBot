021ab7c08a0a5c7994fcafc84f56fa6e38e8d6c6
Yuvira <retrokitty10@gmail.com>
2017-05-27 07:52:17 -0400

    Member count

--- cmds_owner.py
+++ cmds_owner.py
@@ -9,6 +9,7 @@ async def cmds_owner(message, umsg, client, conn):
 	# args/variables
 	args = umsg.split(' ')
 	channel = message.channel
+	server = message.server
 	member = message.author
 	
 	# check owner
@@ -21,13 +22,6 @@ async def cmds_owner(message, umsg, client, conn):
 				await client.send_message(channel, 'User is `' + user.name + '#' + user.discriminator + '`')
 			except:
 				await client.send_message(channel, 'User not found!')
-				
-		# shutdown
-		if (args[0] == 'shutdown'):
-			await client.send_message(channel, '*Shutting down...*')
-			conn.close()
-			await client.logout()
-			sys.exit(0)
 			
 		# list servers
 		if (args[0] == 'serverlist'):
@@ -35,3 +29,17 @@ async def cmds_owner(message, umsg, client, conn):
 			for server in client.servers:
 				msg = msg + '\n`' + server.name + '` owned by `' + server.owner.name + '#' + server.owner.discriminator + '`'
 			await client.send_message(channel, msg)
+			
+		# count members in server
+		if (args[0] == 'usercount'):
+			t = 0
+			for member in server.members:
+				t += 1
+			await client.send_message(channel, 'There are ' + t + ' users in this server')
+				
+		# shutdown
+		if (args[0] == 'shutdown'):
+			await client.send_message(channel, '*Shutting down...*')
+			conn.close()
+			await client.logout()
+			sys.exit(0)
