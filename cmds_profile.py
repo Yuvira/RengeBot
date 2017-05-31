1860146af8f74c79b2b0bc855719ffd8304f5c4e
Yuvira <retrokitty10@gmail.com>
2017-05-31 06:24:10 -0400

    Changing profiles to make room for currency

--- cmds_profile.py
+++ cmds_profile.py
@@ -2,6 +2,23 @@
 import discord
 import sqlite3
 
+# create blank profile
+async def create_profile(member, conn, cur):
+	t = (member.id,)
+	cur.execute('SELECT * FROM profiles WHERE id=?', t)
+	t = cur.fetchone()
+	if (t == None):
+		profile = (member.id, member.name + '#' + member.discriminator, 'Nothing to see here', 0)
+		cur.execute('INSERT INTO profiles VALUES (?,?,?,?)', profile)
+		conn.commit()
+	t = (member.id,)
+	cur.execute('SELECT * FROM ratelimits WHERE id=?', t)
+	t = cur.fetchone()
+	if (t == None):
+		ratelimit = (member.id, member.name + '#' + member.discriminator, 0, 0)
+		cur.execute('INSERT INTO ratelimits VALUES (?,?,?,?)', ratelimit)
+		conn.commit()
+
 # info cmds
 async def cmds_profile(message, umsg, client, conn, cur):
 	
@@ -11,25 +28,29 @@ async def cmds_profile(message, umsg, client, conn, cur):
 	member = message.author
 	
 	# create profile if not exist
-	t = (member.id,)
-	cur.execute('SELECT * FROM profiles WHERE id=?', t)
-	t = cur.fetchone()
-	if (t == None):
-		profile = (member.id, member.name + '#' + member.discriminator, 'Nothing to see here')
-		cur.execute('INSERT INTO profiles VALUES (?,?,?)', profile)
-		conn.commit()
+	await create_profile(member, conn, cur)
 	
 	# profile
 	if (args[0] == 'profile'):
 		
-		# load data
-		t = (member.id,)
-		cur.execute('SELECT * FROM profiles WHERE id=?', t)
-		profile = cur.fetchone()
-		data = list(profile)
+		# show other person's profile
+		if (len(args) == 2 and len(message.mentions) > 0):
+			await create_profile(message.mentions[0])
+			embed = discord.Embed(title=message.mentions[0].name + "'s profile", type='rich', description=data[2], colour=message.mentions[0].colour)
+			if (message.mentions[0].avatar_url == ''):
+				embed.set_thumbnail(url=message.mentions[0].default_avatar_url)
+			else:
+				embed.set_thumbnail(url=message.mentions[0].avatar_url)
+			await client.send_message(channel, content=None, embed=embed)
 				
 		# profile commands
-		if (len(args) > 1):
+		elif (len(args) > 1):
+		
+			# load data
+			t = (member.id,)
+			cur.execute('SELECT * FROM profiles WHERE id=?', t)
+			profile = cur.fetchone()
+			data = list(profile)
 			
 			# description set
 			if (args[1] == 'desc'):
@@ -39,6 +60,13 @@ async def cmds_profile(message, umsg, client, conn, cur):
 				else:
 					await client.send_message(channel, 'You need to enter a description!')
 		
+			# save profile
+			t = (member.id,)
+			profile = tuple(data)
+			cur.execute('DELETE FROM profiles WHERE id=?', t)
+			cur.execute('INSERT INTO profiles VALUES (?,?,?,?)', profile)
+			conn.commit()
+		
 		# show profile
 		else:
 			embed = discord.Embed(title=member.name + "'s profile", type='rich', description=data[2], colour=member.colour)
@@ -47,10 +75,3 @@ async def cmds_profile(message, umsg, client, conn, cur):
 			else:
 				embed.set_thumbnail(url=member.avatar_url)
 			await client.send_message(channel, content=None, embed=embed)
-		
-		# save profile
-		t = (member.id,)
-		profile = tuple(data)
-		cur.execute('DELETE FROM profiles WHERE id=?', t)
-		cur.execute('INSERT INTO profiles VALUES (?,?,?)', profile)
-		conn.commit()
