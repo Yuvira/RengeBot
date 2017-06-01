95bf4b44cef73be5a93d09f738d10e91bc69d0e9
Yuvira <retrokitty10@gmail.com>
2017-06-01 18:49:10 -0400

    Loot & Daily

--- cmds_profile.py
+++ cmds_profile.py
@@ -1,5 +1,6 @@
 # import
 import discord
+import time
 import sqlite3
 
 # create blank profile
@@ -33,6 +34,22 @@ async def save_profile(member, data, conn, cur):
 	profile = tuple(data)
 	cur.execute('DELETE FROM profiles WHERE id=?', t)
 	cur.execute('INSERT INTO profiles VALUES (?,?,?,?)', profile)
+	conn.commit()
+		
+# load profile
+async def load_ratelimit(member, conn, cur):
+	t = (member.id,)
+	cur.execute('SELECT * FROM ratelimits WHERE id=?', t)
+	ratelimit = cur.fetchone()
+	data = list(ratelimit)
+	return data
+	
+# save profile
+async def save_ratelimit(member, data, conn, cur):
+	t = (member.id,)
+	ratelimit = tuple(data)
+	cur.execute('DELETE FROM ratelimits WHERE id=?', t)
+	cur.execute('INSERT INTO ratelimits VALUES (?,?,?,?)', ratelimit)
 	conn.commit()
 
 # info cmds
@@ -94,3 +111,64 @@ async def cmds_profile(message, umsg, client, conn, cur):
 			else:
 				embed.set_thumbnail(url=member.avatar_url)
 			await client.send_message(channel, content=None, embed=embed)
+			
+	# daily
+	if (args[0] == 'daily'):
+		
+		# check ratelimit
+		rl = await load_ratelimit(member, conn, cur)
+		t = int(time.time()) - rl[2]
+		if (t > 86400):
+		
+			# add 100 credits to profile
+			data = await load_profile(member, conn, cur)
+			data[3] = data[3] + 100
+			await save_profile(member, data, conn, cur)
+			
+			# update ratelimit
+			rl[2] = int(time.time())
+			await save_ratelimit(member, rl, conn, cur)
+			await client.send_message('You have received your daily 100 credits!')
+			
+		# show time remaining
+		else:
+			msg = "Slow down! Your daily credits aren't ready yet!\nYou can get your next daily in "
+			t = 86400 - t
+			msg = msg + str(int(t/3600)) + ' hours, '
+			t = t % 3600
+			msg = msg + str(int(t/60)) + ' minutes, '
+			t = t % 60
+			msg = msg + str(t) + ' seconds'
+			await client.send_message(channel, msg)
+			
+	# loot
+	if (args[0] == 'loot'):
+		
+		# check ratelimit
+		rl = await load_ratelimit(member, conn, cur)
+		t = int(time.time()) - rl[3]
+		if (t > 300):
+		
+			# get random credits and update profile
+			loot = int(random.random() * 40) - 10
+			if (loot > 0):
+				data = await load_profile(member, conn, cur)
+				data[3] = data[3] + loot
+				await save_profile(member, data, conn, cur)
+			
+			# update ratelimit
+			rl[3] = int(time.time())
+			await save_ratelimit(member, rl, conn, cur)
+			if (loot > 0):
+				await client.send_message('You looted a whole ' + str(loot) + ' credits!')
+			else:
+				await client.send_message("You couldn't find anything to loot")
+			
+		# show time remaining
+		else:
+			msg = "Slow down! You just looted not that long ago!\nYou can loot again in "
+			t = 300 - t
+			msg = msg + str(int(t/60)) + ' minutes, '
+			t = t % 60
+			msg = msg + str(t) + ' seconds'
+			await client.send_message(channel, msg)
