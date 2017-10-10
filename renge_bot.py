4b991095aba81c52abea49d2c006ed56d61f7ed2
Yuvira <retrokitty10@gmail.com>
2017-10-10 10:44:29 -0400

    Prefix in game message

--- renge_bot.py
+++ renge_bot.py
@@ -80,7 +80,7 @@ async def on_ready():
 	print(client.user.name)
 	print(client.user.id)
 	print('------')
-	await client.change_presence(game=discord.Game(type=0, name='%help | Nyanpasuuu~'), status=None, afk=False)
+	await client.change_presence(game=discord.Game(type=0, name='$help | Nyanpasuuu~'), status=None, afk=False)
 	cur.execute('DELETE FROM games')
 	conn.commit()
 
