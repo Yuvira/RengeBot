8a203f72818ad9da523d8fa568cbb90e746618bd
Yuvira <retrokitty10@gmail.com>
2019-12-19 10:35:04 -0500

    DiscordPY update + formatting consistency

--- cmds_owner.py
+++ cmds_owner.py
@@ -11,19 +11,19 @@ async def cmds_owner(message, umsg, client, conn, cur):
 	# args/variables
 	args = umsg.split(' ')
 	channel = message.channel
-	server = message.server
+	server = message.guild
 	member = message.author
 	
 	# check owner
-	if (member.id == '188663897279037440'):
+	if (member.id == 188663897279037440):
 			
 		# count members in server
 		if (args[0].lower() == 'usercount'):
-			await client.send_message(channel, 'Total users: ' + str(len(server.members)) + '\nBot users: ' + str(len([member for member in server.members if member.bot])))
+			await channel.send('Total users: ' + str(len(server.members)) + '\nBot users: ' + str(len([member for member in server.members if member.bot])))
 			
 		# count total severs
 		if (args[0].lower() == 'servercount'):
-			await client.send_message(channel, 'Total servers: ' + str(len(client.servers)) + '\nBot servers: ' + str(len([server for server in client.servers if len([member for member in server.members if member.bot]) > len([member for member in server.members if not member.bot])])))
+			await channel.send('Total servers: ' + str(len(client.guilds)) + '\nBot servers: ' + str(len([server for server in client.guilds if len([member for member in server.members if member.bot]) > len([member for member in server.members if not member.bot])])))
 			
 		# add song to table
 		if (args[0].lower() == 'addsotd'):
@@ -32,18 +32,18 @@ async def cmds_owner(message, umsg, client, conn, cur):
 				t = (sdata[0], sdata[1], sdata[2], sdata[3])
 				cur.execute('INSERT INTO sotd VALUES (?,?,?,?)', t)
 				conn.commit()
-				await client.send_message(channel, 'Added ' + sdata[1] + ' by ' + sdata[0] + ' to the song table!')
+				await channel.send('Added ' + sdata[1] + ' by ' + sdata[0] + ' to the song table!')
 			except:
-				await client.send_message(channel, 'You did something wrong!')
+				await channel.send('You did something wrong!')
 		
 		# force next song
 		if (args[0].lower() == 'updatesotd'):
 			try:
 				cur.execute('DELETE FROM sotd WHERE rowid=(SELECT MIN(rowid) FROM sotd)')
 				conn.commit()
-				await client.send_message(channel, 'Song of the Day successfully updated!')
+				await channel.send('Song of the Day successfully updated!')
 			except:
-				await client.send_message(channel, 'Something went wrong!')
+				await channel.send('Something went wrong!')
 		
 		# list tracks
 		if (args[0].lower() == 'listsotd'):
@@ -55,24 +55,24 @@ async def cmds_owner(message, umsg, client, conn, cur):
 					break
 				else:
 					msg = msg + '\n' + str(a+1) + ': ' + t[0] + ' - ' + t[1]
-			embed = discord.Embed(title='SOTD List', type='rich', description=msg)
-			await client.send_message(channel, content=None, embed=embed)
+			embed = discord.Embed(title = 'SOTD List', type = 'rich', description = msg)
+			await channel.send(content = None, embed = embed)
 		
 		# give credits
 		if (args[0].lower() == 'givecredits'):
 			try:
-				user = await client.get_user_info(args[1])
+				user = await client.fetch_user(args[1])
 				t = int(args[2])
 				data = await load_profile(user, conn, cur)
 				data[3] = str(int(data[3]) + t)
 				await save_profile(data, conn, cur)
-				await client.send_message(channel, 'Gave ' + args[2] + ' credits to ' + str(user) + '!')
+				await channel.send('Gave ' + args[2] + ' credits to ' + str(user) + '!')
 			except:
-				await client.send_message(channel, 'You did something wrong!')
+				await channel.send('You did something wrong!')
 				
 		# shutdown
 		if (args[0].lower() == 'shutdown'):
-			await client.send_message(channel, '*Shutting down...*')
+			await channel.send('*Shutting down...*')
 			conn.close()
 			await client.logout()
 			sys.exit(0)
